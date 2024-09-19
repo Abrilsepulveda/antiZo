cde761137b5cb002dfc811614dbee07e6b22431e
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-19 00:27:25 -0300

    agregue usuarioAdd y empresaAdd en +not-found.tsx

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,6 +1,8 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
+import UsuarioAdd from '../app/usuarios/components/usuarioAdd';
+import EmpresaAdd from '../app/usuarioEmpresa/components/EmpresaAdd';
 
 export default function NotFoundScreen() {
   return (
@@ -8,7 +10,9 @@ export default function NotFoundScreen() {
       <Stack.Screen options={{ title: 'UUUUUY!' }} />
       <View style={styles.container}>
         <Text style={styles.title}>Esta pantalla no existe</Text>
-        
+        <UsuarioAdd onAdd={undefined}></UsuarioAdd>
+        <EmpresaAdd onAdd={undefined}></EmpresaAdd>
+
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Pantalla principal</Text>
         </Link>
