9192c369c1d25778cac6d0b1f7d159167995fbce
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-22 22:52:53 -0300

    agregué import de firebase en UsuariosApp

--- src/modules/login/personas/components/UsuariosAdd.jsx
+++ src/modules/login/personas/components/UsuariosAdd.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import UsuarioAdd from "../components/usuarioAdd";
 import UsuarioList from "../components/usuarioList";
 import useUsuario from "../hooks/useUsuario";
 import firebase from "firebase/app";
+import "firebase/auth";
 import "firebase/firestore";
 
 const UsuarioApp = () => {
@@ -41,3 +41,4 @@ const UsuarioApp = () => {
           loginWithRedirect();
         }
       };
+
