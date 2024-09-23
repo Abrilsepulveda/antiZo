9292054100e40f8dfa88891a2f13bdc719ec6a97
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-22 23:52:34 -0300

    agrego cambios de login

--- src/modules/login/personas/components/UsuariosAdd.jsx
+++ src/modules/login/personas/components/UsuariosAdd.jsx
@@ -4,7 +4,7 @@ import { Text , Stylesheet , View } from "react-native"
 export default function UsuarioAdd() {
     ruturn(
         <view>
-            <text> textInComponent</text>
+            <text> login usuarios </text>
         </view>
     );
 }
