83bc5ac279661643f468822aacd3b1f457f1e229
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-22 23:50:49 -0300

    también le estoy agregando lo de drawer navigation en ambos login de usuarios y empresas

---
+++ src/modules/login/empresas/components/EmpresaAdd.jsx
@@ -0,0 +1,11 @@
+import React, { component } from 'react'
+import { Text , Stylesheet , View } from "react-native"
+
+export default function EmpresaAdd() {
+    ruturn(
+        <view>
+            <text> login empresas </text>
+        </view>
+    );
+}
+const styles = StyleSheet.create({});
