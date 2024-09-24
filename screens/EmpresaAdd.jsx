9878981e9904ff540b3fe44c1ac7eb95733a1fde
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-24 17:03:51 -0300

    agregué const db y export RegistroEmpresa en EmpresaAdd.jsx

--- screens/EmpresaAdd.jsx
+++ screens/EmpresaAdd.jsx
@@ -1,3 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { auth, createUserWithEmailAndPassword } from './firebase';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+
+const db = getFirestore();
+export default function RegistroEmpresa({ navigation }) {
+
