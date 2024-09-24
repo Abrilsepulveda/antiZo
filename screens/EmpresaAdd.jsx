0e9d2613288f95a5a75c833c6a42c7a17e565364
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-24 17:08:22 -0300

    agregué const dentro de export en EmpresaAdd.jsx

--- screens/EmpresaAdd.jsx
+++ screens/EmpresaAdd.jsx
@@ -5,4 +5,9 @@ import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
 const db = getFirestore();
 export default function RegistroEmpresa({ navigation }) {
-
+    const [nombreEmpresa, setNombreEmpresa] = useState('');
+    const [tipoEmpresa, setTipoEmpresa] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [contacto, setContacto] = useState('');
+     
