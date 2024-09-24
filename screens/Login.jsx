816175897480e116dd25181a6f02a6afc4d42dbb
luciana <carmona.lucianaa.epet20@gmail.com>
2024-09-24 17:00:21 -0300

    agregue export LoginScreen

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native'; 
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from './firebase'; // Importa la configuración de Firebase
+import { auth } from './firebase'; 
+
+
+export default function LoginScreen({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+}
