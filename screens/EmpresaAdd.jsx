75c4723b7ad815563006bf9e8debed60c6c4a0b8
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-24 20:13:56 -0300

    Agregando boton de registro

--- screens/EmpresaAdd.jsx
+++ screens/EmpresaAdd.jsx
@@ -68,3 +68,7 @@ export default function RegistroEmpresa({ navigation }) {
                     onChangeText={setContacto}
                     keyboardType="phone-pad"
             />
+
+        <TouchableOpacity style={styles.button} onPress={handleRegistro}>
+        <Text style={styles.buttonText}>Registrarme</Text>
+        </TouchableOpacity>
