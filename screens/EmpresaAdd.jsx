fbe30a2fecdbf8141485e900c4be336f1276bc18
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-24 16:55:08 -0300

    agregué otros import

---
+++ screens/EmpresaAdd.jsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { auth, createUserWithEmailAndPassword } from './firebase';
