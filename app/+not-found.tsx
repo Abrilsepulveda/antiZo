9c1e223c406a75508bdff1f45d1351bfd34c15af
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-25 03:14:23 -0300

    Agregué home a index

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,6 +7,7 @@ import Login from '../screens/Login';
 import UsuariosAdd from '../screens/UsuariosAdd';
 import EmpresasAdd from '../screens/EmpresaAdd';
 import Busqueda from '../screens/Busqueda';
+import home from '../screens/home';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,7 @@ export default function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="UsuariosAdd" component={UsuariosAdd} />
         <Stack.Screen name="EmpresasAdd" component={EmpresasAdd} />
+        <Stack.Screen name='home' component={home} />
         <Stack.Screen name="Busqueda" component={Busqueda} />
       </Stack.Navigator>
     </NavigationContainer>
