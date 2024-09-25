f2698e8cb149fbb6655b780e1d0e1c12f2f16cf3
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-25 00:18:24 -0300

    Importando las pantallas al index

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -2,3 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// Importar las pantallas
+import Login from './Login';
+import UsuariosAdd from './UsuariosAdd';
+import EmpresasAdd from './EmpresasAdd';
+import Home from './Home';
+import Busqueda from './Busqueda';
