ae074581dde61f3f578ed5f764d8316dac74a1e3
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-25 00:20:00 -0300

    Agregando pantallas en stack

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -10,3 +10,18 @@ import Home from './Home';
 import Busqueda from './Busqueda';
 
 const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {/* Configurar las rutas para cada pantalla */}
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="UsuariosAdd" component={UsuariosAdd} />
+        <Stack.Screen name="EmpresasAdd" component={EmpresasAdd} />
+        <Stack.Screen name="Busqueda" component={Busqueda} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
