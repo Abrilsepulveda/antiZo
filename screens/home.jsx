979ca5aa8100754440fca35a27ada90be5448ad8
diorellacarente <diorella.carente.epet20@gmail.com>
2024-09-25 02:03:15 -0300

    agregue un return y mas codigo

--- screens/home.jsx
+++ screens/home.jsx
@@ -2,3 +2,21 @@
 import React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native'; // Componentes de React Native
 import MapView, { Marker } from 'react-native-maps'; // Mapa y marcador de React Native 
+Maps
+
+export default function MapScreen()  {
+    // Defini la región inicial que mostrará el mapa, con coordenadas y zoom
+    const initialRegion = {
+      latitude: -34.603722, // Coordenada de latitud inicial
+      longitude: -58.381592, // Coordenada de longitud inicial
+      latitudeDelta: 0.0922, // Nivel de zoom en la latitud
+      longitudeDelta: 0.0421, // Nivel de zoom en la longitud
+    };
+    return (
+        //principal 
+        <View style={styles.container}>
+      {/* Componente MapView para mostrar el mapa */}
+      <MapView
+        style={styles.map} // Estilos del mapa para ocupar toda la pantalla
+        initialRegion={initialRegion} // Configuramos la región inicial
+      >
