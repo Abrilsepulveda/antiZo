8097ab32df2977ca810337659f2d84861dbd6dbc
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-24 23:23:52 -0300

    Hice cambios en index

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,10 +1,10 @@
-import './gesture-handler';
 
 import { Link, Stack } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import UsuariosAdd from '../screens/UsuariosAdd';
 import EmpresaAdd from '../screens/EmpresaAdd';
+import Login from '../screens/Login'
 import 'react-native-gesture-handler';
 
 import { createStackNavigator } from '@react-navigation/stack';
