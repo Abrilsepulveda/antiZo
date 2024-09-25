9be9069b0a07321aae543396436e818f6d273e9e
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-24 22:02:14 -0300

    Borré sin querer cosas de login, ya las agregué

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native'; 
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {  signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from './firebase'; 
 
 
