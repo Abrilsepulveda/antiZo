ef456d051e89ee26a64b432476a378ed6a90993c
Abril <sepulveda.abril.epet20@gmail.com>
2024-09-25 00:17:40 -0300

    Conectando bien las pantallas

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,37 +1,4 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
-import UsuariosAdd from '../screens/UsuariosAdd';
-import EmpresaAdd from '../screens/EmpresaAdd';
-import Login from '../screens/Login'
-import 'react-native-gesture-handler';
-
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',   
-
-    
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
